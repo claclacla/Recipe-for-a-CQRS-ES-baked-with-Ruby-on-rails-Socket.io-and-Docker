98c9f2db4b4bb2368bc78157dba5508d070ded34
Adelchino Simone <seconddesire@gmail.com>
2019-01-31 21:59:51 +0100

    Removed unused routes from products and orders api servers

--- microservices/products/api-server/config/routes.rb
+++ microservices/products/api-server/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :order_products
-  resources :orders
   resources :products
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
